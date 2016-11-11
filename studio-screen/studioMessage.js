cb767a63038db8ee6aec95350ca6e624789849eb
Dominic Smith <dom@domsmith.co.uk>
2016-11-11 08:44:39 +0000

    Armistice OB instructions

--- studio-screen/studioMessage.js
+++ studio-screen/studioMessage.js
@@ -1,3 +1,3 @@
 displayMessage({
-	"message": ""
+	"message": "<font size=\"12pt\">Armistice day<br>10:58:50 - Run news intro, read strap, remote up<br>10:59:00 - OB feed on Remote<br>Approx 11:10:00 - Out words &quot;I'm Julian Clover&quot;<br>Remote down, weather jingle, read weather</font>"
 });
