9c0f38eaf53e34d031874a7b5e1163f97161ac5f
Robert Chipperfield <robert@syxis.co.uk>
2016-11-11 22:56:33 +0000

    Update studioMessage.js

--- studio-screen/studioMessage.js
+++ studio-screen/studioMessage.js
@@ -1,3 +1,3 @@
 displayMessage({
-	"message": "<font style=\"font-size:0.6em\">Armistice day<br>10:58:50 - Run news intro, read strap, remote up<br>10:59:00 - OB feed on Remote<br>Approx 11:10:00 - Out words &quot;I'm Julian Clover&quot;<br>Remote down, weather jingle, read weather</font>"
+	"message": ""
 });
