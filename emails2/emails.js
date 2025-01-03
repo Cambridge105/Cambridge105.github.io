ced5c0743b307e57ebb262a1ef293d817ec775db
Dominic Smith <dom@domsmith.co.uk>
2025-01-03 08:21:45 +0000

    Debug

--- emails2/emails.js
+++ emails2/emails.js
@@ -108,8 +108,10 @@ function parseMsg(obj) {
     }
     else if ((date.indexOf(" ago") > 1) || (date=="Now"))
     {
+        console.log("here " + obj.Uid);
         classOverride = "new";
     }
+    console.log("Date [" + date + "] and classOverride [" + classOverride + "] for " + obj.Uid);
     html = "<article class=\"msg " + classOverride + "\" id=\"" + obj.Uid + "\"><div class=\"msg_meta\"><div class=\"msg_meta_from\">From: <span class=\"msg_meta_value\">"  + sender + "</span></div> <div class=\"msg_meta_via\">via: <span class=\"msg_meta_value\">" + method + "</span></div> <div class=\"msg_meta_time\">Time: <span class=\"msg_meta_value\" id=\"" + obj.Uid + "-ds\">" + date + "</span></div><input type=\"hidden\" id=\"" + obj.Uid + "-ts\" value=\"" + obj.DateString + "\"></div>";
     html += "<div id=\"" + obj.Uid + "-body\">" + formatMsgBody(obj.BodySummary, obj.BodyFull) + "</div>";
     html += "<div class=\"msg_actions\"><button onClick=\"markRead(" + obj.Uid + ")\" id=\"" + obj.Uid + "-rb\">Read</button> </div></article>"
